fcb2a482dbf67cd7586fbaaabe2f5a0830901d07
Victor Ribeiro Garcia <133285806+vribeirog@users.noreply.github.com>
2025-09-27 15:56:52 -0300

    Add files via upload

--- cgSolver.c
+++ cgSolver.c
@@ -4,8 +4,20 @@
 #include <math.h>
 #include <time.h>
 
+#include "utils.h"
+#include "sislin.h"
 
 int main() {
-    
+    srandom(20252);
+
+    int n, k, maxit;
+    real_t omega, epsilon;
+
+    scanf("%d", &n);
+    scanf("%d", &k);
+    scanf("%lf", &omega);
+    scanf("%d", &maxit);
+    scanf("%lf", &epsilon);
+
     return 0;
 }
